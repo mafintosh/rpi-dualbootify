fd765849c373fad273c0c7d34ac7c73d38416706
Mathias Buus <mathiasbuus@gmail.com>
2017-11-17 19:08:48 +0100

    support --extra-bytes and --extra-sectors

--- bin.sh
+++ bin.sh
@@ -3,13 +3,32 @@
 set -e
 
 if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ]; then
-  echo "Usage: rpi-dualbootify <image-one.img> <image-two.img> <dual.img>"
+  echo "Usage: rpi-dualbootify <image-one.img> <image-two.img> <dual.img> [options]"
+  echo
+  echo "  --extra-sectors <expand-image-two-with-these-sectors>"
+  echo "  --extra-bytes   <same-as-above-but-in-bytes>"
+  echo
   exit 1
 fi
 
+EXTRA_SECTORS=0
 IMAGE_ONE="$1"
 IMAGE_TWO="$2"
 IMAGE_DUAL="$3"
+
+shift
+shift
+shift
+
+while true; do
+  case "$1" in
+    --extra-sectors) EXTRA_SECTORS=$2; shift; shift ;;
+    --extra-bytes)   EXTRA_SECTORS=$(($2 / 512)); shift; shift ;;
+    *)               break ;;
+  esac
+done
+
+echo "extra $EXTRA_SECTORS"
 
 parse_image () {
   local IFS=$'\n'
@@ -26,6 +45,7 @@ parse_image () {
 parse_image IMAGE_ONE $IMAGE_ONE
 parse_image IMAGE_TWO $IMAGE_TWO
 
+IMAGE_TWO_ROOT_SECTORS=$(($IMAGE_TWO_ROOT_SECTORS + $EXTRA_SECTORS))
 FDISK_CMD="$FDISK_CMD""n\np\n1\n$IMAGE_ONE_BOOT_START\n+$IMAGE_ONE_BOOT_SECTORS\n"
 FDISK_CMD="$FDISK_CMD""n\np\n2\n$IMAGE_ONE_ROOT_START\n+$IMAGE_ONE_ROOT_SECTORS\n"
 
@@ -60,5 +80,13 @@ dd skip=$IMAGE_ONE_ROOT_START seek=$IMAGE_ONE_ROOT_START count=$(($IMAGE_ONE_ROO
 echo Copying image two...
 dd skip=$IMAGE_TWO_BOOT_START seek=$IMAGE_TWO_BOOT_LOGICAL_START count=$(($IMAGE_TWO_BOOT_SECTORS + 1)) ibs=512 obs=512 if=$IMAGE_TWO of=$IMAGE_DUAL conv=notrunc 2>/dev/null
 dd skip=$IMAGE_TWO_ROOT_START seek=$IMAGE_TWO_ROOT_LOGICAL_START count=$(($IMAGE_TWO_ROOT_SECTORS + 1)) ibs=512 obs=512 if=$IMAGE_TWO of=$IMAGE_DUAL conv=notrunc 2>/dev/null
+
+if [ "$EXTRA_SECTORS" != "0" ]; then
+  echo Resizing image two...
+  DEVICE=$(sudo losetup -f --show "$IMAGE_DUAL" --offset=$((512 * IMAGE_TWO_ROOT_LOGICAL_START)))
+  sudo e2fsck -p -f $DEVICE >/dev/null
+  sudo resize2fs $DEVICE >/dev/null 2>/dev/null
+  sudo losetup -d $DEVICE >/dev/null
+fi
 
 echo Done. Run fdisk -l $IMAGE_DUAL to see the joint partitions
